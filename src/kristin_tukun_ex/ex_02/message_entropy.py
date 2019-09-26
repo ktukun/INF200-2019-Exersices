46a0cf8e8bc0ce78905cea9e6dbe65d6e4ae683b
Kristin <kristin.tukun@nmbu.no>
2019-09-26 14:53:21 +0200

    Initial task B

---
+++ src/kristin_tukun_ex/ex_02/message_entropy.py
@@ -0,0 +1,8 @@
+
+def entropy(message):
+    pass
+
+
+if __name__ == "__main__":
+    for msg in '', 'aaaa', 'aaba', 'abcd', 'This is a short text.':
+        print('{:25}: {:8.3f} bits'.format(msg, entropy(msg)))
