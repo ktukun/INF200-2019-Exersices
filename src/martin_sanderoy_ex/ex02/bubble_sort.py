57b3be0367c55564fc1eaed317e5b88a7c4654d8
Martin Sanderøy <martsand@nmbu.no>
2019-09-26 16:06:16 +0200

    Task 02 C, exercise 02 is completed

---
+++ src/martin_sanderoy_ex/ex02/bubble_sort.py
@@ -0,0 +1,21 @@
+
+
+def bubble_sort(data):
+    typo = type(data)
+    data = list(data)
+    n = len(data)
+    for i in range(n-1):
+        for j in range(0, n - i - 1):
+            if data[j] > data[j + 1]:
+                data[j], data[j+1] = data[j+1], data[j]
+    return typo(data)
+
+
+if __name__ == "__main__":
+
+    for data in ((),
+                 (1,),
+                 (1, 3, 8, 12),
+                 (12, 8, 3, 1),
+                 (8, 3, 12, 1)):
+        print('{!s:>15} --> {!s:>15}'.format(data, bubble_sort(data)))
