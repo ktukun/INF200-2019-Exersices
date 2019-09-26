d18833c56be5ed00f274621e83364356d22bb23c
Kristin <kristin.tukun@nmbu.no>
2019-09-26 18:51:56 +0200

    Completing task B
    I'm just using the counter function in the function letter freq because we dont need to open a text file in this program

--- src/kristin_tukun_ex/ex_02/message_entropy.py
+++ src/kristin_tukun_ex/ex_02/message_entropy.py
@@ -1,6 +1,17 @@
+def letter_freq(txt):
+    from collections import Counter
+    return Counter(txt.lower())
+
 
 def entropy(message):
-    pass
+    from math import log2
+    counted_message = letter_freq(message)
+    n = len(counted_message)
+    h = []
+    for n_i in counted_message:
+        p_i = counted_message[n_i] / n
+        h.append(-p_i*log2(p_i))
+    return sum(h)
 
 
 if __name__ == "__main__":
