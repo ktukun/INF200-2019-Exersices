f74bd0c696df10f83b79502592516839a1be544a
Kristin <kristin.tukun@nmbu.no>
2019-09-27 10:12:56 +0200

    Adding coments to the message_entropy function and changing some variable names

--- src/kristin_tukun_ex/ex_02/message_entropy.py
+++ src/kristin_tukun_ex/ex_02/message_entropy.py
@@ -1,16 +1,27 @@
 def letter_freq(txt):
+    """
+    This is a function with text as input, it will then count occurrence of letters and signs in the text. It does not
+    differentiate on upper an lowercase letters.
+    :param txt: A text string
+    :return: A list of number of occurrences.
+    """
     from collections import Counter
     return Counter(txt.lower())
 
 
 def entropy(message):
+    """
+    A function that calculates the entropy h of a message.
+    :param message: A string of text
+    :return: The calculated entropy h
+    """
     from math import log2
     counted_message = letter_freq(message)
-    n = sum(counted_message.values())
+    length_message = sum(counted_message.values())
     h = []
-    for n_i in counted_message:
-        p_i = counted_message[n_i] / n
-        h.append(-p_i*log2(p_i))
+    for occurences in counted_message:
+        freq = counted_message[occurences] / length_message
+        h.append(-freq*log2(freq))
     return sum(h)
 
 
