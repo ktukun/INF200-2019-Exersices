4bdd6611395aad5e7da5d877404d25cafb066ef3
Kristin <kristin.tukun@nmbu.no>
2019-10-02 16:20:32 +0200

    Completed task A
    Wrote all the test functions for the bubble_sort script

--- src/kristin_tukun_ex/ex03_project/test_sorting.py
+++ src/kristin_tukun_ex/ex03_project/test_sorting.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from kristin_tukun_ex.ex_02.bubble_sort import bubble_sort
+import random
+import numpy as np
+import pytest
 
 __author__ = 'Kristin Tukun'
 __email__ = 'kritu@nmbu.no'
@@ -6,14 +10,12 @@ __email__ = 'kritu@nmbu.no'
 
 def test_empty():
     """Test that the sorting function works for empty list"""
-
-
-pass
+    assert bubble_sort([]) == []
 
 
 def test_single():
     """Test that the sorting function works for single-element list"""
-    pass
+    assert bubble_sort([1]) == [1]
 
 
 def test_sorted_is_not_original():
@@ -28,7 +30,9 @@ def test_sorted_is_not_original():
     Now sorted_data shall be a different object than data,
     not just another name for the same object.
     """
-    pass
+    data = [3, 2, 1]
+    sorted_data = bubble_sort(data)
+    assert data is not sorted_data
 
 
 def test_original_unchanged():
@@ -42,22 +46,24 @@ def test_original_unchanged():
 
     Now data shall still contain [3, 2, 1].
     """
-    pass
+    data = [3, 2, 1]
+    bubble_sort(data)
+    assert data is data
 
 
 def test_sort_sorted():
     """Test that sorting works on sorted data."""
-    pass
+    assert bubble_sort([1, 2, 3]) == [1, 2, 3]
 
 
 def test_sort_reversed():
     """Test that sorting works on reverse-sorted data."""
-    pass
+    assert bubble_sort([3, 2, 1]) == [1, 2, 3]
 
 
 def test_sort_all_equal():
     """Test that sorting handles data with identical elements."""
-    pass
+    assert bubble_sort([1, 1, 1]) == [1, 1, 1]
 
 
 def test_sorting():
@@ -68,4 +74,28 @@ def test_sorting():
     ensure that they are sorted correctly. These could be lists of
     numbers of different length or lists of strings.
     """
-    pass
+    """Test if the function sorts list_1 that is a list of integers with 
+    positive and negative values from -50 to 50 and with a random length of 
+    list from 2 to 100. We are testing 20 different lists.
+    """
+
+    for _ in range(20):
+        list_1 = np.random.randint(low=-50, high=50,
+                                   size=(random.randint(2, 100)))
+        sorted_list_1 = bubble_sort(list_1)
+
+        for small, large in zip(sorted_list_1[:-1], sorted_list_1[1:]):
+            assert small <= large
+
+    """Test if the function sorts list_1 that is a list of integers with 
+    positive and negative values from -75 to 75 and with a random length of 
+    list from 2 to 100. We are testing 20 different lists.
+    """
+
+    for _ in range(20):
+        list_2 = np.random.uniform(low=-75, high=75,
+                                   size=(random.randint(2, 100)))
+        sorted_list_2 = bubble_sort(list_2)
+
+        for small, large in zip(sorted_list_2[:-1], sorted_list_2[1:]):
+            assert small <= large
