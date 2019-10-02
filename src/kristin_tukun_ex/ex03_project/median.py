1d6f9c0426832472c6f4534bf3cb37d046612f0c
Kristin <kristin.tukun@nmbu.no>
2019-10-02 16:26:50 +0200

    Making a file for the median function

---
+++ src/kristin_tukun_ex/ex03_project/median.py
@@ -0,0 +1,12 @@
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sorted_data = sorted(data)
+    n = len(sorted_data)
+    return (sorted_data[n//2] if n % 2 == 1
+            else 0.5 * (sorted_data[n//2 - 1] + sorted_data[n//2]))
