50773f335f851011a7829aa1c78935d04c42cc3d
Kristin <kristin.tukun@nmbu.no>
2019-10-25 15:53:49 +0200

    Made a skeleton for the walker class

---
+++ src/kristin_tukun_ex/ex_04/walker.py
@@ -0,0 +1,18 @@
+import random
+
+class Walker:
+
+    def __init__(self, start_position, home_position):
+         self.start = start_position
+         self.home = home_position
+         self.position = 0
+
+    def move(self):
+        number = random.randint(0,1)
+        if number >
+
+    def is_at_home(self):
+
+    def get_position(self):
+
+    def get_steps(self):
