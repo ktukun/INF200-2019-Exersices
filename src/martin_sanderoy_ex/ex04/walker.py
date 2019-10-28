64ce4fdd6c5e07f9ad176b89ea9b9820019b85ea
Martin Sanderøy <martsand@nmbu.no>
2019-10-28 15:07:26 +0100

    added a little explanatory text to functions

--- src/martin_sanderoy_ex/ex04/walker.py
+++ src/martin_sanderoy_ex/ex04/walker.py
@@ -13,6 +13,7 @@ class Walker:
         self.steps = 0
 
     def move(self):
+        """Equal chance to move backwards and forwards by 1 step"""
         self.steps += 1
         if random.randint(0, 1) == 0:
             self.position -= 1
@@ -20,15 +21,19 @@ class Walker:
             self.position += 1
 
     def is_at_home(self):
+        """Check if walker is at his home"""
         return self.position == self.home
 
     def get_position(self):
+        """returns current position"""
         return self.position
 
     def get_steps(self):
+        """returns number of steps taken"""
         return self.steps
 
     def walk_home(self):
+        """walks until the walker is home"""
         while Walker.is_at_home(self) is False:
             Walker.move(self)
 
