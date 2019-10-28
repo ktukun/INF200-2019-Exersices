9b2cd3f693f2a56339d9c489b093f53451896dd2
Kristin <kristin.tukun@nmbu.no>
2019-10-28 11:58:25 +0100

    First version of the walker function

--- src/kristin_tukun_ex/ex_04/walker.py
+++ src/kristin_tukun_ex/ex_04/walker.py
@@ -1,18 +1,29 @@
 import random
 
+
 class Walker:
 
     def __init__(self, start_position, home_position):
-         self.start = start_position
-         self.home = home_position
-         self.position = 0
+        self.position = start_position
+        self.home = home_position
+        self.steps = 0
 
     def move(self):
-        number = random.randint(0,1)
-        if number >
+        self.steps += 1
+        step = random.randint(0, 1)
+        if step == 0:
+            self.position -= 1
+        else:
+            self.position += 1
+        return self.position
 
     def is_at_home(self):
+        return self.position == self.home
 
     def get_position(self):
+        return self.position
 
     def get_steps(self):
+        return self.steps
+
+
