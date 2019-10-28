caa612ba77fdaa30a8ea7024aa111ef7fb6b2253
Martin Sanderøy <martsand@nmbu.no>
2019-10-28 13:17:01 +0100

    code runs, but print format missing

--- src/martin_sanderoy_ex/ex04/walker.py
+++ src/martin_sanderoy_ex/ex04/walker.py
@@ -9,6 +9,7 @@ class Walker:
     def __init__(self, initial_position, home_position):
         self.position = initial_position
         self.home = home_position
+        self.distance = abs(initial_position - home_position)
         self.steps = 0
 
     def move(self):
@@ -20,10 +21,7 @@ class Walker:
         return self.position
 
     def is_at_home(self):
-        if self.position == self.home:
-            return True
-        else:
-            return False
+        return self.position == self.home
 
     def get_position(self):
         return self.position
@@ -31,5 +29,21 @@ class Walker:
     def get_steps(self):
         return self.steps
 
+    def walk_home(self):
+        while Walker.is_at_home(self) is False:
+            Walker.move(self)
+
+
+if __name__ == '__main__':
+    start = 0
+    home = [1, 2, 5, 10, 20, 50, 100]
+    for home in home:
+        students = []*5
+        distance = abs(home-start)
+        for _ in range(5):
+            student = Walker(start, home)
+            student.walk_home()
+            students.append(student.get_steps())
+        print('Distance:  ',distance, students)
 
 
