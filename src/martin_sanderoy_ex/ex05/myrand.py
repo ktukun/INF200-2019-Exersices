f3280c2a186f49c12244ee39c7959f58fb331099
Martin Sanderøy <martsand@nmbu.no>
2019-11-04 13:54:26 +0100

    made code for RandIter

--- src/martin_sanderoy_ex/ex05/myrand.py
+++ src/martin_sanderoy_ex/ex05/myrand.py
@@ -14,22 +14,56 @@ class LCGRand:
         self.random_number = self.a * self.random_number % self.m
         return self.random_number
 
-
-class ListRand:
-    def __init__(self, number_list):
-        self.random_numbers = number_list.copy()
-
-    def rand(self):
-        if len(self.random_numbers) == 0:
-            raise RuntimeError
-
-        return self.random_numbers.pop(0)
+    def random_sequence(self, length):
+        return RandIter(self, length)
 
 
-if __name__ == '__main__':
-    lcg = LCGRand(1)
-    print('two numbers from LCGRand: {0} and {1}'.format(lcg.rand(),
-                                                         lcg.rand()))
-    lr = ListRand([4, 5, 29, 11])
-    print('two numbers from ListRand: {0} and {1}'.format(lr.rand(),
-                                                          lr.rand()))
+class RandIter:
+    def __init__(self, random_number_generator, length):
+        """
+        Arguments
+        ---------
+        random_number_generator :
+            A random number generator with a ``rand`` method that
+            takes no arguments and returns a random number.
+        length : int
+            The number of random numbers to generate
+        """
+        self.generator = random_number_generator
+        self.length = int(length)
+        self.num_generated_numbers = None
+
+    def __iter__(self):
+        """
+        Initialise the iterator.
+        Returns
+        -------
+        self : RandIter
+        Raises
+        ------
+        RuntimeError
+            If iter is called twice on the same RandIter object.
+        """
+        self.num_generated_numbers = 0
+        return self
+
+    def __next__(self):
+        """
+        Generate the next random number.
+        Returns
+        -------
+        int
+        A random number.
+        Raises
+        ------
+        RuntimeError
+            If the ``__next__`` method is called before ``__iter__``.
+        StopIteration
+            If ``self.length`` random numbers are generated.
+        """
+        if self.num_generated_numbers is None:
+            raise RuntimeError(f'{type(self)} is not initialised')
+        if self.num_generated_numbers >= self.length:
+            raise StopIteration
+        self.num_generated_numbers += 1
+        return self.generator
