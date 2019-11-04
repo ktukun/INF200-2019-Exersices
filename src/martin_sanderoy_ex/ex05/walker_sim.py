c10c484405ddb3eeb5b76bb7442a7d2f79d38029
Martin Sanderøy <martsand@nmbu.no>
2019-11-04 11:09:31 +0100

    completed task A

--- src/martin_sanderoy_ex/ex05/walker_sim.py
+++ src/martin_sanderoy_ex/ex05/walker_sim.py
@@ -32,8 +32,90 @@ class Walker:
         """returns number of steps taken"""
         return self.steps
 
+    def walk_home(self):
+        """walks until the walker is home"""
+        while self.is_at_home() is False:
+            self.move()
 
-def walk_home(person):
-    """walks until the walker is home"""
-    while Walker.is_at_home(person) is False:
-        Walker.move(person)
+
+class Simulation:
+    def __init__(self, start, home, seed):
+        """
+        Initialise the simulation
+
+        Arguments
+        ---------
+        start : int
+            The walker's initial position
+        home : int
+            The walk ends when the walker reaches home
+        seed : int
+            Random generator seed
+        """
+        self.start = start
+        self.home = home
+        self.seed = seed
+
+    def single_walk(self):
+        """
+        Simulate single walk from start to home, returning number of steps.
+
+        Returns
+        -------
+        int
+            The number of steps taken
+        """
+        walk = Walker(self.start, self.home)
+        walk.walk_home()
+        return walk.get_steps()
+
+    def run_simulation(self, num_walks):
+        """
+        Run a set of walks, returns list of number of steps taken.
+
+        Arguments
+        ---------
+        num_walks : int
+            The number of walks to simulate
+
+        Returns
+        -------
+        list[int]
+            List with the number of steps per walk
+        """
+        random.seed(self.seed)
+        walks = []*num_walks
+        for _ in range(num_walks):
+            walks.append(self.single_walk())
+        return walks
+
+
+if __name__ == '__main__':
+    """
+    The main section of the script shall simulate
+    -  20 walks from start 0 to home 10
+    -  20 walks from start 10 to home 0
+    -  for each of those cases simulate *twice* with seed value 12345 and
+       *once* with seed value 54321
+    -  print the resulting lists (six lists in total).
+    """
+    num_sims = 20
+    sim = Simulation(0, 10, 12345)
+    print('20 simulation with start:0, home:10, seed:12345. ',
+          sim.run_simulation(num_sims))
+    sim = Simulation(0, 10, 12345)
+    print('20 simulation with start:0, home:10, seed:12345. ',
+          sim.run_simulation(num_sims))
+    sim = Simulation(0, 10, 54321)
+    print('20 simulation with start:0, home:10, seed:54321. ',
+          sim.run_simulation(num_sims))
+
+    sim = Simulation(10, 0, 12345)
+    print('20 simulation with start:10, home:0, seed:12345. ',
+          sim.run_simulation(num_sims))
+    sim = Simulation(10, 0, 12345)
+    print('20 simulation with start:10, home:0, seed:12345. ',
+          sim.run_simulation(num_sims))
+    sim = Simulation(10, 0, 54321)
+    print('20 simulation with start:10, home:0, seed:54321. ',
+          sim.run_simulation(num_sims))
